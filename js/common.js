067104326f4bf90674c48a18ece2baad18224fb7
sasho <tochcha@gmail.com>
2016-11-11 10:47:13 +0200

    ubral lishnee

--- js/common.js
+++ js/common.js
@@ -1,38 +1,9 @@
 $(document).ready(function() {
-document.createElement('header');
-document.createElement('nav');
-document.createElement('section');
-document.createElement('article');
-document.createElement('aside');
-document.createElement('footer');
-document.createElement('figure');
-document.createElement('figcaption');
-document.createElement('main');
-    
     
     	// анимация сэндвича
 	$(".select-buttons span").click(function () {
 		$(".select-buttons span").removeClass("active");
 		$(this).toggleClass("active");
-	});
-	
-	// слайдер
-	$('.main-slider').slick({
-		infinite: true,
-		dots: true,
-		fade: true,
-		arrows: false,
-		pauseOnFocus: false,
-		speed: 1000,
-		autoplay: true,
-		responsive: [
-			{
-			  breakpoint: 768,
-			  settings: {
-				fade: false
-			  }
-			}
-		]
 	});
 	
 	
